c3a4c77246189b7c811c74790c12b14b09589c1c
Tess Phillips <tess.scarlet@gmail.com>
2023-03-09 18:00:16 +0000

    js / live editor working

--- moviedatascript.js
+++ moviedatascript.js
@@ -34,146 +34,164 @@ let movieData = {
     },
   };
 
-let movieNameList = document.getElementById("myMovieNameList")
-movieData.forEach((item)=>{
-  let li = document.createElement("li");
-  li.innerText = item;
-  movieNameList.appendChild(li);
-})
+// let movieNameList = document.getElementById("myMovieNameList")
+// movieData.forEach((item)=>{
+//   let li = document.createElement("li");
+//   li.innerText = item;
+//   movieNameList.appendChild(li);
+// })
 
-createTableMain()
+// createTableMain()
 
-function createTableMain() {
+// function createTableMain() {
 
-  var headers = ["Movie Name", "Rating", "Runtime", "Year"];
-  var table = document.createElement("TABLEMAIN");  //makes a table element for the page
+//   var headers = ["Movie Name", "Rating", "Runtime", "Year"];
+//   var table = document.createElement("TABLEMAIN");  //makes a table element for the page
       
-  for(var i = 0; i < movieData.length; i++) {
-      var row = table.insertRow(i);
-      row.insertCell(0).innerHTML = movieData[i]; // was books[i].title but our list nested so will be dif
-      // for(var j = 0; j = 5; j++) { // do we need a nested loop here? 
-      row.insertCell(1).innerHTML = movieData[i].rating;
-      row.insertCell(2).innerHTML = movieData[i].runtime;
-      row.insertCell(3).innerHTML = movieData[i].year;
-  }
+//   for(var i = 0; i < movieData.length; i++) {
+//       var row = table.insertRow(i);
+//       row.insertCell(0).innerHTML = movieData[i]; // was books[i].title but our list nested so will be dif
+//       // for(var j = 0; j = 5; j++) { // do we need a nested loop here? 
+//       row.insertCell(1).innerHTML = movieData[i].rating;
+//       row.insertCell(2).innerHTML = movieData[i].runtime;
+//       row.insertCell(3).innerHTML = movieData[i].year;
+//   }
 
-  var header = table.createTHead();
-  var headerRow = header.insertRow(0);
-  for(var i = 0; i < headers.length; i++) {
-      headerRow.insertCell(i).innerHTML = headers[i];
-  }
+//   var header = table.createTHead();
+//   var headerRow = header.insertRow(0);
+//   for(var i = 0; i < headers.length; i++) {
+//       headerRow.insertCell(i).innerHTML = headers[i];
+//   }
 
-  document.body.append(table);
-}
+//   document.body.append(table);
+// }
 
-function sortTableStr(n, tablename) { 
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById(tablename);
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
+// function createTablePlot() {
 
-function sortTableNum(n, tablename) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById(tablename);
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /*Make a loop that will continue until
-  no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      //check if the two rows should switch place based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (Number(x.innerHTML) > Number(y.innerHTML)) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (Number(x.innerHTML) < Number(y.innerHTML)) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-    }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
+//   var headers = ["Movie Name", "Plot"];
+//   var table = document.createElement("TABLEPLOT");  //makes a table element for the page
+      
+//   for(var i = 0; i < movieData.length; i++) {
+//       var row = table.insertRow(i);
+//       row.insertCell(0).innerHTML = movieData[i]; // was books[i].title but our list nested so will be dif
+//       // for(var j = 0; j = 5; j++) { // do we need a nested loop here? 
+//       row.insertCell(1).innerHTML = movieData[i].plot;
+//   }
 
-let x="hi";
-document.getElementById("x").innerHTML = x;
-console.log(x)
+//   var header = table.createTHead();
+//   var headerRow = header.insertRow(0);
+//   for(var i = 0; i < headers.length; i++) {
+//       headerRow.insertCell(i).innerHTML = headers[i];
+//   }
 
-document.getElementById("demo").innerHTML = allMovieNames;
+//   document.body.append(table);
+// }
+
+// function sortTableStr(n, tablename) { 
+//   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+//   table = document.getElementById(tablename);
+//   switching = true;
+//   // Set the sorting direction to ascending:
+//   dir = "asc";
+//   /* Make a loop that will continue until
+//   no switching has been done: */
+//   while(switching){
+//     // Start by saying: no switching is done:
+//     switching = false;
+//     rows = table.rows;
+//     /* Loop through all table rows (except the
+//     first, which contains table headers): */
+//     for (i = 1; i < (rows.length - 1); i++) {
+//       // Start by saying there should be no switching:
+//       shouldSwitch = false;
+//       /* Get the two elements you want to compare,
+//       one from current row and one from the next: */
+//       x = rows[i].getElementsByTagName("TD")[n];
+//       y = rows[i + 1].getElementsByTagName("TD")[n];
+//       /* Check if the two rows should switch place,
+//       based on the direction, asc or desc: */
+//       if (dir == "asc") {
+//         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+//           // If so, mark as a switch and break the loop:
+//           shouldSwitch = true;
+//           break;
+//         }
+//       } else if (dir == "desc") {
+//         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+//           // If so, mark as a switch and break the loop:
+//           shouldSwitch = true;
+//           break;
+//         }
+//       }
+//     }
+//     if (shouldSwitch) {
+//       /* If a switch has been marked, make the switch
+//       and mark that a switch has been done: */
+//       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+//       switching = true;
+//       // Each time a switch is done, increase this count by 1:
+//       switchcount ++;
+//     } else {
+//       /* If no switching has been done AND the direction is "asc",
+//       set the direction to "desc" and run the while loop again. */
+//       if (switchcount == 0 && dir == "asc") {
+//         dir = "desc";
+//         switching = true;
+//       }
+//     }
+//   }
+// }
+
+// function sortTableNum(n, tablename) {
+//   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+//   table = document.getElementById(tablename);
+//   switching = true;
+//   // Set the sorting direction to ascending:
+//   dir = "asc";
+//   /*Make a loop that will continue until
+//   no switching has been done:*/
+//   while (switching) {
+//     //start by saying: no switching is done:
+//     switching = false;
+//     rows = table.rows;
+//     /*Loop through all table rows (except the
+//     first, which contains table headers):*/
+//     for (i = 1; i < (rows.length - 1); i++) {
+//       //start by saying there should be no switching:
+//       shouldSwitch = false;
+//       /*Get the two elements you want to compare,
+//       one from current row and one from the next:*/
+//       x = rows[i].getElementsByTagName("TD")[n];
+//       y = rows[i + 1].getElementsByTagName("TD")[n];
+//       //check if the two rows should switch place based on the direction, asc or desc: */
+//       if (dir == "asc") {
+//         if (Number(x.innerHTML) > Number(y.innerHTML)) {
+//           //if so, mark as a switch and break the loop:
+//           shouldSwitch = true;
+//           break;
+//         }
+//       } else if (dir == "desc") {
+//         if (Number(x.innerHTML) < Number(y.innerHTML)) {
+//           //if so, mark as a switch and break the loop:
+//           shouldSwitch = true;
+//           break;
+//     }
+//     if (shouldSwitch) {
+//       /*If a switch has been marked, make the switch
+//       and mark that a switch has been done:*/
+//       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+//       switching = true;
+//       // Each time a switch is done, increase this count by 1:
+//       switchcount ++;
+//     } else {
+//       /* If no switching has been done AND the direction is "asc",
+//       set the direction to "desc" and run the while loop again. */
+//       if (switchcount == 0 && dir == "asc") {
+//         dir = "desc";
+//         switching = true;
+//       }
+//     }
+//   }
+// }
+// let tests = "one";
+// document.getElementById("test").innerHTML = tests;
